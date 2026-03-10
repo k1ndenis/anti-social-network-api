92827f9956b9e93a2eeb784958e8b59f3cd4ad90
k1ndenis <d3n0s4vr@gmail.com>
2026-03-10 17:15:17 +0400

    feat: serve static files from public folder

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 5000;
 
